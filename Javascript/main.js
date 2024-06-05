127e32ba33efd83c8f79bb8eef8e3b19dcc71e5a
Khushi Sinha <ks1393990@gmail.com>
2024-06-05 14:12:18 +0530

    learning basic JS

---
+++ Javascript/main.js
@@ -0,0 +1,23 @@
+// var i = 10;
+// i = 100;
+// console.log(i);
+
+
+// let firstname = "Khus";
+// age = 21;
+// console.log("Name is"+" "+ firstname + " "+ "and age is"+" "+ age );
+
+
+// let person = "asdfg";
+// if(person === "girl"){
+//     console.log("Good Morning mam");
+// }
+// else{
+//     console.log("Good Morning sir");
+// }
+
+let ans =0;
+for(let i=0; i<100000000; i=i+1){
+    ans=ans+1;
+}
+console.log("Answer is" + ans);
