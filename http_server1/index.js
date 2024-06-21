245859d2e69045d819d40a78dea5baa31c989a77
Khushi Sinha <ks1393990@gmail.com>
2024-06-21 23:31:51 +0530

    refactor: update route handler in index.js

--- http_server1/index.js
+++ http_server1/index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get("/router_handler", (req, res) => {
+  res.send('Hello Kus to the world of Express!')
 })
 
 app.listen(port, () => {
