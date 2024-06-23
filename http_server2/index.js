c5ae05c8c40cc8cc01a848bd0a025dcdf441f90f
Khushi Sinha <ks1393990@gmail.com>
2024-06-23 22:58:12 +0530

    new commits

--- http_server2/index.js
+++ http_server2/index.js
@@ -2,17 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-// app.get("/route-handler", function(req, res) {
-//     res.json({
-//         Name: "Khus",
-//         Age: 21
-//     })
-//   })
+app.get("/route-handler", function(req, res) {
+    res.json({
+        Name: "Khus",
+        Age: 21
+    })
+  })
 
-// // app.get('/', (req, res) => {
-// //   res.send('Hello World!!')
-// // })
+app.get('/', (req, res) => {
+  res.send('Hello World!!')
+})
 
-// // app.listen(port, () => {
-// //   console.log(`Example app listening on port ${port}`)
-// // })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
