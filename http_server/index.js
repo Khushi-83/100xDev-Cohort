46300d0d9776097d4d0427c2c92ab1b7660f0eaa
Khushi Sinha <ks1393990@gmail.com>
2024-07-11 16:20:59 +0530

    simple http server

--- http_server/index.js
+++ http_server/index.js
@@ -1,9 +1,18 @@
 const http = require("http");
 const fs = require("fs");
 const myServer = http.createServer((req, res) => {
-  const log = `${Date.now()} : New Req Received\n`;  
+  const log = `${Date.now()} ${req.url}: New Req Received\n`;  
     fs.appendFile("log.txt", log, (err, data)=> {
-        res.end("Hello World!!");
+        switch(req.url){
+            case "/": res.end("Hello World!!");
+            break;
+            case "/about": res.end("Hi, I am Khus Sinha");
+            break;
+            case "/contact": res.end("Contact me at sinhakhushi0803@gmail.com");
+            break;
+            default: res.end("404 Page Not Found");     
+        }
+        
     }); 
 });
 
