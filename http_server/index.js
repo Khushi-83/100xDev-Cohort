b20aa46fbb3c26f80433d0245b1a73688f58015e
Khushi Sinha <ks1393990@gmail.com>
2024-07-16 15:24:51 +0530

    refactor: Add URL parsing and query parameter handling in index.js

--- http_server/index.js
+++ http_server/index.js
@@ -1,29 +1,51 @@
-const http = require("http");
-const fs = require("fs");
-const url = require("url");
+// const http = require("http");
+// const fs = require("fs");
+// const url = require("url");
 
-const myServer = http.createServer((req, res) => {
-    if (req.url === "/favicon.ico") return res.end();
-    const log = `${Date.now()} ${req.url}: New Req Received\n`; 
-    const myUrl = url.parse(req.url, true);
-    console.log(myUrl);
-    fs.appendFile("log.txt", log, (err, data)=> {
-        switch(myUrl.pathname  ){
-            case "/": 
-            res.end("Hello World!!");
-            break;
-            case "/about": 
-            const username = myUrl.query.myname;
-            res.end(`Hi, ${username}`);
-            break;
-            case "/search": 
-            const search = myUrl.query.search_query;
-            res.end("Your Search results for" + search);
-            break;
-            default: res.end("404 Page Not Found");     
-        }
+// const myServer = http.createServer((req, res) => {
+//     if (req.url === "/favicon.ico") return res.end();
+//     const log = `${Date.now()} ${req.url}: New Req Received\n`; 
+//     const myUrl = url.parse(req.url, true);
+//     console.log(myUrl);
+//     fs.appendFile("log.txt", log, (err, data)=> {
+//         switch(myUrl.pathname  ){
+//             case "/": 
+//             res.end("Hello World!!");
+//             break;
+//             case "/about": 
+//             const username = myUrl.query.myname;
+//             res.end(`Hi, ${username}`);
+//             break;
+//             case "/search": 
+//             const search = myUrl.query.search_query;
+//             res.end("Your Search results for" + search);
+//             break;
+//             default: res.end("404 Page Not Found");     
+//         }
         
-    }); 
-});
+//     }); 
+// });
 
-myServer.listen(3000, () => console.log("Server Started!"));
+// myServer.listen(3000, () => console.log("Server Started!"));
+
+// Same code in Express JS as the Express Js makes the code clutter free and easy for us to read and understand it.
+// url parser is not needed in express becoz, it can inernally parse the url.
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!!");
+});
+app.get("/about", (req, res) => {
+    const username = req.query.myname;
+    res.send(`Hi, ${username}`);
+});
+app.get("/search", (req, res) => {
+    const search = req.query.search_query;
+    res.send("Your Search results for" + search);
+});
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
