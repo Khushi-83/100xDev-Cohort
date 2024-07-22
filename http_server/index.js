0d611f7c02ba386f87e969eda14cbc0bb1850f34
Khushi Sinha <ks1393990@gmail.com>
2024-07-22 21:59:58 +0530

    refactor: Update MongoDB connection string and add connection status logging in index.js

--- http_server/index.js
+++ http_server/index.js
@@ -72,6 +72,8 @@ const userSchema = new mongoose.Schema({
 });
 
 mongoose.connect('mongodb://127.0.0.1:27017/TestDB ')
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.log("Mongo Error", err));
 const User = mongoose.model("User", userSchema);
 
 app.listen(port, () => {
